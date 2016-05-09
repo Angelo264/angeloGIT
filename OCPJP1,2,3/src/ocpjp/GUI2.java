caa80bbbd72fec383db1185edba0e0857426aacb
Angelo <Angelo@10.0.0.21>
2016-05-09 14:31:17 +0100

    Panel was added and Text field and label was add to panel

--- OCPJP1,2,3/src/ocpjp/GUI2.java
+++ OCPJP1,2,3/src/ocpjp/GUI2.java
@@ -12,15 +12,18 @@ import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JOptionPane;
+import javax.swing.JPanel;
 import javax.swing.JTextField;
 
 //Create top-level 
 @SuppressWarnings("serial")
 public class GUI2 extends JFrame{//Choose between interface inheritance and class inheritance
 	
+	//Jframe object was was unused and unnecessary.
 	private static JTextField tf,tf1,tf2,tf3,tf4;
 	private JButton B1;
 	private JLabel L1,L2,L3,L4;
+	private JPanel P1;
 	static String ID,name,surname,address;
 	
 	static Icon x = new ImageIcon("SAPS.png");
@@ -35,6 +38,9 @@ public class GUI2 extends JFrame{//Choose between interface inheritance and clas
 			super("SAPS");
 			setLayout(new FlowLayout());
 			
+			P1 = new JPanel();
+			P1.setBackground(Color.GREEN);
+			
 			tf = new JTextField();
 			tf.setColumns(10);
 			tf.setText("RECORD HOLDER");
@@ -42,61 +48,60 @@ public class GUI2 extends JFrame{//Choose between interface inheritance and clas
 			tf.setBackground(Color.BLUE);
 			tf.setEditable(false);
 			tf.setAlignmentX(JFrame.TOP_ALIGNMENT);
-			add(tf);
-			//JPanel P1 = new JPanel((LayoutManager) x);
-			//P1.setSize(100,100);
-			//add(P1);
+			P1.add(tf);
+		
 			L1 = new JLabel("ID");
 			L1.setBounds(100, 200, 100, 200);
-			//P1.setVisible(true);
+		
 			add(L1);
 			tf1 = new JTextField();		
 			tf1.setColumns(13);
 			tf1.setToolTipText("ID Number");	
 			tf1.setFont(new Font("Serif",Font.BOLD,12));
 			tf1.setCaretColor(Color.RED);
-			add(tf1);
+			P1.add(tf1);
 			
 			L2 = new JLabel("Name");
 			L2.setBounds(150, 300, 150, 300);	
 			L2.setAlignmentX(150);
 			L2.setAlignmentY(300);
 		
-			add(L2);
+			P1.add(L2);
 			
 			tf2 = new JTextField();		
 			tf2.setColumns(10);
 			tf2.setToolTipText("Enter Name Here");	
 			tf2.setFont(new Font("Serif",Font.ITALIC,12));
 			tf2.setCaretColor(Color.RED);
-			add(tf2);
+			P1.add(tf2);
 			
 			L3 = new JLabel("Surname");		
-			add(L3);
+			P1.add(L3);
 			
 			tf3 = new JTextField();		
 			tf3.setColumns(10);
 			tf3.setToolTipText("Enter Surname Here");	
 			tf3.setFont(new Font("Serif",Font.ITALIC,12));
 			tf3.setCaretColor(Color.RED);			
-			add(tf3);
+			P1.add(tf3);
 			
 			L4 = new JLabel("Address");
 			L4.setAlignmentX(RIGHT_ALIGNMENT);		
-			add(L4);
+			P1.add(L4);
 			
 			tf4 = new JTextField();		
 			tf4.setColumns(10);
 			tf4.setToolTipText("Enter Address Here");	
 			tf4.setFont(new Font("Serif",Font.ITALIC,12));
 			tf4.setCaretColor(Color.RED);
-			add(tf4);
+			P1.add(tf4);
 			
 			B1 = new JButton("Submit",x);
             B1.setRolloverSelectedIcon(b);
 			B1.setToolTipText("Enter Button");				
-			add(B1);	
-		
+			P1.add(B1);	
+			
+			add(P1);
 		/*P1.setBackground(Color.RED);
 		P1.setAlignmentX(800);
 		P1.setAlignmentY(600);*/
